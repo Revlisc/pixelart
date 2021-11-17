0e712dd4686cd93d650dbe412eba56478f19f4e3
Arthur Baker <artbaker82@gmail.com>
2021-11-16 20:47:29 -0800

    grid working

--- src/Components/DrawingWindow/DrawingWindow.js
+++ src/Components/DrawingWindow/DrawingWindow.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 // import Row from "../Row/Row";
 import "./drawingwindow.css";
 
@@ -6,19 +6,39 @@ export const Square = () => {
   return <div className='square'></div>;
 };
 
+const CanvasSizePicker = () => {};
+
 const DrawingWindow = ({ panelLength, selectedColor }) => {
   const [grid, setGrid] = useState();
-  let squares = [];
 
+  const divStyle = {
+    height: "500px",
+    width: "500px",
+    display: "grid",
+    border: "1px solid black",
+    gridTemplateColumns: "repeat(auto-fill, 25px)",
+    gridTemplateRows: "repeat(auto-fill, 25px)",
+  };
+
+  let squares = [];
   useEffect(() => {
-    for (let i = 0; i < panelLength; i++) {
-      squares.push(<Square />);
-    }
+    const generateGrid = () => {
+      for (let i = 0; i < 400; i++) {
+        squares.push(<Square />);
+      }
+    };
+    generateGrid();
     setGrid(squares);
   }, []);
 
   console.log(grid);
-  return <div className='containerDrawing'>{grid}</div>;
+  return (
+    <Fragment>
+      <div className='containerDrawing' style={divStyle}>
+        {grid}
+      </div>
+    </Fragment>
+  );
 };
 
 export default DrawingWindow;

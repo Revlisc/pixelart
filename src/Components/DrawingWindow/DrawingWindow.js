cd9036663ef0dc29cb55dfa5d963d1ef58e72d4c
Arthur Baker <artbaker82@gmail.com>
2021-11-18 12:01:48 -0800

    color scheme and color state refractored

--- src/Components/DrawingWindow/DrawingWindow.js
+++ src/Components/DrawingWindow/DrawingWindow.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, Fragment } from "react";
 // import Row from "../Row/Row";
 import "./drawingwindow.css";
 import Square from "../Square/Square";
-import ColorScheme from "../ColorScheme/ColorScheme";
 
 import { v4 as uuidv4 } from "uuid";
 
@@ -14,71 +13,11 @@ const getWindowDimentions = () => {
   };
 };
 
-// const ColorPicker = ({ addNewColor }) => {
-//   const [color, setColor] = useState("");
-//   //color picker is a controlled component, when state changes, pass the color value to the parent state selectedColor
-//   // useEffect(() => {
-//   //   addNewColor(color);
-//   // }, [color]);
-//   const handleChangeComplete = (color) => {
-//     setColor(color.hex);
-//     console.log("change complete");
-//   };
-
-//   const colorPickerSample = {
-//     height: "30px",
-//     width: "30px",
-//     borderRadius: "25%",
-//     backgroundColor: `${color}`,
-//   };
-
-//   return (
-//     <Fragment>
-//       <div className='color-picker'>
-//         <ChromePicker color={color} onChangeComplete={handleChangeComplete} />
-//         <div style={colorPickerSample}></div>
-//         <div className='add-color' onClick={() => addNewColor(color)}>
-//           Add this
-//         </div>
-//       </div>
-//     </Fragment>
-//   );
-// };
-
-// const ColorScheme = ({ handleColorChange }) => {
-//   const [pallete, setPallete] = useState(["#bada55", "#A6B7DA"]);
-//   const [showColorPicker, setShowColorPicker] = useState(false);
-
-//   const addNewColor = (color) => {
-//     setPallete([...pallete, color]);
-//   };
-
-//   return (
-//     <Fragment>
-//       <div className='color-pallete'>
-//         {pallete.map((color, idx) => {
-//           return (
-//             <div
-//               key={idx}
-//               className='color-item'
-//               style={{ backgroundColor: `${color}` }}
-//               onClick={() => handleColorChange(color)}></div>
-//           );
-//         })}
-//         <div className='add-new-color' onClick={() => setShowColorPicker(!showColorPicker)}>
-//           Add new color
-//         </div>
-//       </div>
-//       {showColorPicker && <ColorPicker addNewColor={addNewColor} />}
-//     </Fragment>
-//   );
-// };
-
-const DrawingWindow = () => {
+const DrawingWindow = ({ selectedColor }) => {
   const [grid, setGrid] = useState();
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimentions());
   const [canvasWidth, setCanvasWidth] = useState("");
-  const [selectedColor, setSelectedColor] = useState("");
+  // const [selectedColor, setSelectedColor] = useState("");
 
   const getCanvasWidth = () => {
     //should be 75% of viewport height?
@@ -122,10 +61,6 @@ const DrawingWindow = () => {
     getCanvasWidth();
   }, [windowDimensions]);
 
-  const handleColorChange = (color) => {
-    setSelectedColor(color);
-  };
-
   return (
     <Fragment>
       <div className='container-drawingWindow'>
@@ -136,10 +71,6 @@ const DrawingWindow = () => {
               return <Square key={idx} id={uuidv4()} selectedColor={selectedColor} />;
             })}
         </div>
-      </div>
-      <div className='colors-container'>
-        {/* <ColorPicker handleColorChange={handleColorChange} /> */}
-        <ColorScheme handleColorChange={handleColorChange} />
       </div>
     </Fragment>
   );
--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,32 +1,37 @@
-import React, {useState} from 'react'
-import DrawingWindow from '../DrawingWindow/DrawingWindow'
-import './Panel.css'
-import FilePicker from '../FilePicker/FilePicker'
+import React, { useState } from "react";
+import DrawingWindow from "../DrawingWindow/DrawingWindow";
+import "./Panel.css";
+import FilePicker from "../FilePicker/FilePicker";
+import ColorScheme from "../ColorScheme/ColorScheme";
 
 const Panel = () => {
-    const [panelLength, setPanelLength] = useState(160)
-    const [selectedColor, setSelectedColor] = useState('#f44332')
-    const [images, setImages] = useState({imageGuides: []})
+  const [panelLength, setPanelLength] = useState(160);
+  const [selectedColor, setSelectedColor] = useState("#f44332");
+  const [images, setImages] = useState({ imageGuides: [] });
 
-    const updateUploadedFiles = (files) => setImages({...images, imageGuides: files})
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
-    return (
-        <div className='container'>
-            <div className='left'>
-                <form onSubmit={handleSubmit}>
-                    <FilePicker accept='.jpg,.png,.jpeg' label='Images' updateFilesCb={updateUploadedFiles} />
-                </form>
-            </div>
-            <div className='right'>
-                <DrawingWindow panelLength={panelLength} />
-            
-            </div>
-        </div>
-        
-    )
-}
+  const updateUploadedFiles = (files) => setImages({ ...images, imageGuides: files });
 
-export default Panel
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color);
+  };
+
+  return (
+    <div className='container'>
+      <div className='left'>
+        <form onSubmit={handleSubmit}>
+          <FilePicker accept='.jpg,.png,.jpeg' label='Images' updateFilesCb={updateUploadedFiles} />
+        </form>
+        <ColorScheme handleColorChange={handleColorChange} />
+      </div>
+      <div className='right'>
+        <DrawingWindow selectedColor={selectedColor} />
+      </div>
+    </div>
+  );
+};
+
+export default Panel;

1b9c3158833a49df6c6b9c2059f0fcbd35e602af
Arthur Baker <artbaker82@gmail.com>
2021-11-19 16:18:38 -0800

    color extractor working...with bug

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import DrawingWindow from "../DrawingWindow/DrawingWindow";
 import "./Panel.css";
 import FilePicker from "../FilePicker/FilePicker";
 import ColorScheme from "../ColorScheme/ColorScheme";
+import { ColorExtractor } from "react-color-extractor";
 
 const Panel = () => {
-  const [panelLength, setPanelLength] = useState(160);
   const [selectedColor, setSelectedColor] = useState("#f44332");
   const [images, setImages] = useState({ imageGuides: [] });
-
-  const updateUploadedFiles = (files) => setImages({ ...images, imageGuides: files });
+  const [extractedColors, setExtractedColors] = useState("");
+  console.log(extractedColors);
+  const updateUploadedFiles = (files) => {
+    setImages({ ...images, imageGuides: files });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,19 +22,33 @@ const Panel = () => {
     setSelectedColor(color);
   };
 
+  let url = "";
+  if (images.imageGuides.length > 0) {
+    let blob = URL.createObjectURL(images.imageGuides[0]);
+    url = blob;
+  }
   return (
-    <div className='container'>
-      <div className='left'>
-        <form onSubmit={handleSubmit}>
-          <FilePicker accept='.jpg,.png,.jpeg' label='Images' updateFilesCb={updateUploadedFiles} />
-        </form>
+    <Fragment>
+      <h1 className='dashTitle'>LET'S MAKE SOME GLOW ART</h1>
 
-        <ColorScheme handleColorChange={handleColorChange} selectedColor={selectedColor} />
+      <div className='container'>
+        <div className='left'>
+          <form onSubmit={handleSubmit}>
+            <FilePicker
+              accept='.jpg,.png,.jpeg'
+              label='Images'
+              updateFilesCb={updateUploadedFiles}
+            />
+          </form>
+          <ColorExtractor src={url} getColors={(colors) => setExtractedColors(colors)} />;
+          {/* <img src={url}></img> */}
+          <ColorScheme handleColorChange={handleColorChange} selectedColor={selectedColor} />
+        </div>
+        <div className='right'>
+          <DrawingWindow selectedColor={selectedColor} />
+        </div>
       </div>
-      <div className='right'>
-        <DrawingWindow selectedColor={selectedColor} />
-      </div>
-    </div>
+    </Fragment>
   );
 };
 

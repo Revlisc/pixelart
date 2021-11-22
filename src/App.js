a1d38c5c6c7fca490fe2de5cb3aa39805fcc18bb
dales <dalesimmons0210@gmail.com>
2021-11-22 12:05:33 -0800

    test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import Panel from './Components/Panel/Panel'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import LandingPage from './Pages/landingPage';
+//import LandingPage from './Pages/landingPage';
 
 function App() {
-  
+  //<Route path='/' element={<LandingPage />} />
   return (
     <BrowserRouter >
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/panel' element={<Panel />} />
+        
+        <Route path='/' element={<Panel />} />
         
         
       </Routes>
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,11 +13,11 @@ const Contact = () => {
           name='Dale Simmons'
           img={dalepic}
           text={
-            ("Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            ["Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
             " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ",
             "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ",
-            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+            "in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]
           }
         />
         <Card
